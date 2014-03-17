415f94e2d67c119776b7ba4bc11850fbc7d0e95d
andresjordanze <andresjordanze@gmail.com>
2014-03-17 11:21:43 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Informaticacomp::Application.config.secret_key_base = 'ef3c1d478c289a0c95cd14550a159576fdef6eb3deb6e5cb2c37b8ea19b941a67431192649052a5dabc9c8ef4f35f86b534e834352fe53e890498587a86929c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Informaticacomp::Application.config.session_store :cookie_store, key: '_informaticacomp_session'
