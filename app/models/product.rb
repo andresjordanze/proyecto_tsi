274353bfecdc4a2f2e2c65036bbe22375932572a
Oscar Sergio Gonzalez Camacho <oscar_sgc@hotmail.com>
2014-03-19 09:03:29 -0400

    Funciona subproductos

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,10 @@
 class Product < ActiveRecord::Base
-<<<<<<< HEAD
-	attr_accessible :name, :detail, :description, :code, :general_code, :brand, :category, :bought_price, :sale_price, :created_at, :updated_at,:photo
+has_many :subproducts, dependent: :destroy
+	attr_accessible :name, :detail, :description, :quantity, :general_code, :brand, :category, :bought_price, :sale_price, :created_at, :updated_at,:photo
 	has_attached_file :photo
  	validates_attachment_presence :photo
 	validates_attachment_size :photo, :less_than => 5.megabytes
 	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
-=======
-	has_many :subproducts, dependent: :destroy
-	attr_accessible :name, :detail, :description, :quantity, :general_code, :brand, :category, :bought_price, :sale_price, :created_at, :updated_at
 
 	after_create :crear_subproductos
 
@@ -16,6 +13,4 @@ class Product < ActiveRecord::Base
     		subproducts.create("code" => general_code+"-#{num}")
       	end
     end
-
->>>>>>> b09bcd00b365915f6f912066c0f949a405c1cb18
 end
