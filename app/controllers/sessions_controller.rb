1d622afb51442722fc42f7de228b366c86e402b8
Oscar Sergio Gonzalez Camacho <oscar_sgc@hotmail.com>
2014-03-24 10:28:11 -0400

    Layout mejorado

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:login], params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to_target_or_default root_url, :notice => "Sesion Iniciada Correctamente."
+      #redirect_to_target_or_default root_url, :notice => "Sesion Iniciada Correctamente."
+      redirect_to root_url
     else
       flash.now[:alert] = "Login o Contraseña Invalido."
       render :action => 'new'
