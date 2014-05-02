ca57ecb9d8e76656f88221305419ebd61d4c3731
Usuario <patty_rosembluth@ubuntu.(none)>
2014-05-02 10:25:19 -0400

    Editar pedido

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -66,12 +66,25 @@ class OrdersController < ApplicationController
     	return items
   	end
 
-
-
 	def destroy
 		@order = Order.find(params[:id])
 		@order.destroy
 		redirect_to orders_url
+	end
+
+	def edit
+		@order = Order.find(params[:id])
+	end
+
+	def update
+		@order = Order.find(params[:id])
+		if @order.update_attributes(params[:order])
+			@order.save
+			flash[:success] = "Orden Actualizada"
+			redirect_to @order			
+		else
+			render 'edit'
+		end
 	end
 
 end
