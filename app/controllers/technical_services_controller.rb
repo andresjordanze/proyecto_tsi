c080ddcd4acb416ee073c90f213bbcb8d303f1c7
Oscar Sergio Gonzalez Camacho <oscar_sgc@hotmail.com>
2014-07-02 00:35:40 -0400

    Fixeado

--- app/controllers/technical_services_controller.rb
+++ app/controllers/technical_services_controller.rb
@@ -60,11 +60,12 @@ class TechnicalServicesController < ApplicationController
 	end
 
 	def search
-		@value = params[:name]
-    	#@technical_services = buscar(@value)
-		@technical_services = TechnicalService.where("client like ? or id like ?", "%#{params[:name]}%", "%#{params[:name]}%").paginate(:per_page => 5, :page => params[:page])
+		#@value = params[:name]
+    	#@technical_services = buscar(@value).
+    	@technical_services = buscar(params[:name]).paginate(:page => params[:page], :per_page => 6 )
+		#@technical_services = TechnicalService.where("client like ? or id like ?", "%#{params[:name]}%", "%#{params[:name]}%").paginate(:per_page => 5, :page => params[:page])
     	#@technical_services = TechnicalService.where("id = :id or client = :client", {id: params[:name], client: params[:name]}).to_a
-    	#render 'index'
+    	render 'index'
   	end
 
   	def buscar(valor)
