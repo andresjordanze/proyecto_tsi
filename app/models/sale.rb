51ffa4ae1bd1384206b1138c0c9110c6e7678993
andresjordanze <andresjordanze@gmail.com>
2014-07-02 04:04:25 -0400

    sale validations

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -17,7 +17,7 @@ class Sale < ActiveRecord::Base
 	
 	validates :check_number, presence: {:message => "Usted debe ingresar el numero de factura"}	
 	validates :check_number, numericality: {:message => "El valor del numero de factura debe ser numerico"}
-	validates :check_number, length: {minimum: 4, maximum: 12, :message => "El numero de factura debe tener minimo 4 caracteres"}
+	validates :check_number, length: {minimum: 4, :message => "El numero de factura debe tener minimo 4 caracteres"}
 	validates :check_number, numericality: {greater_than: 0, :message => "El numero de factura no puede ser menor a 0" }
 	validates :check_number, uniqueness: {case_sensitive: false, :message => "El numero de factura ya existe"}
 	
